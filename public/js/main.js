7944a4f2f68bab79625ace1a01b974142286f251
Sanjeev Nayak <snayak@springbot.com>
2016-12-16 13:47:44 -0500

    moved framework comments to the readme

--- public/js/main.js
+++ public/js/main.js
@@ -1,13 +1,3 @@
-/***********************************************************************************************************************
- * This example uses the Angular framework. Frameworks in general are helpful because they offer a lot of
- * robust functionality to help you build an application without having to build all the smaller building blocks first.
- * Angular specifically offers a lot of useful binding methods that can allow you to change data in your Javascript code,
- * and have the HTML view update to match the new data automatically.
- * However, relying on frameworks can obfuscate what is actually going on in a web app, and can make it difficult
- * if you ever need to do something that the framework doesn't already handle out of the box. It's important
- * to keep that trade-off in mind when choosing what framework to use.
- ************************************************************************************************************************/
-
 // Initialize our angular app, and add a controller to it.
 angular
   .module('MyApp', [])
